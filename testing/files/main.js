3c0e4cfa64facb719671c4722263aa616c6daa97
klevinC <30991818+klevinC@users.noreply.github.com>
2018-11-01 11:05:18 -0600

    Update main.js

--- testing/files/main.js
+++ testing/files/main.js
@@ -1,9 +1,9 @@
 var img;
 function preload() {
-  //img = loadImage('files/unicorn.jpg');
+  img = loadImage('files/unicorn.jpg');
 }
 function setup() {
   createCanvas(600, 600);
   background(0)
-  //image(img, 0, 0);
+  image(img, 0, 0);
 }
